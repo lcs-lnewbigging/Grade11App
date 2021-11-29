a2846ac6ae2422b88abc5aa705dd47c4c65c1cfc
Luke Newbigging <lnewbigging@lcs.on.ca>
2021-11-29 14:26:14 -0500

    Initial Commit

---
+++ LcsDiningHallApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LcsDiningHallApp
+//
+//  Created by Luke Newbigging on 2021-11-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LcsDiningHallApp/LcsDiningHallAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LcsDiningHallAppApp.swift
+//  LcsDiningHallApp
+//
+//  Created by Luke Newbigging on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct LcsDiningHallAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
