4f0211e277adc418a8cc30873da0484ca229ea4d
Luke Newbigging <lnewbigging@lcs.on.ca>
2021-12-02 08:35:44 -0500

    Having a bug on the static view where the image and text is not showing up

--- LcsDiningHallApp/DetailView.swift
+++ LcsDiningHallApp/DetailView.swift
@@ -15,7 +15,11 @@ struct DetailView: View {
                 .font(.title)
             VStack{
                 HStack{
-                    Image("Grilled Cheese Pic")
+                    Image("grilledCheese")
+                    
+                    
+                    Text("A toasted sandwich, grilled cheese sandwich, cheese toastie, or grilled cheese is a hot sandwich (toastie) made with one or more varieties of cheese (a cheese sandwich) on bread. It is typically prepared by heating one or more slices of cheese between slices of bread, with a cooking fat such as butter, on a frying pan, griddle, or sandwich toaster, until the bread browns and the cheese melts. A grilled cheese may contain ingredients besides the bread and cheese, as long as the latter is the main focus.")
+                    
                     
         
         
@@ -26,6 +30,9 @@ struct DetailView: View {
 }
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
+        NavigationView {
         DetailView()
+                .navigationBarHidden(true)
+        }
     }
 }
